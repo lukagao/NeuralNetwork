e6dbf7713fac46432596c0f8ab48d38d730cd344
lukagao <790794090@qq.com>
2019-06-13 21:38:44 +0800

    add data

---
+++ conf.py
@@ -0,0 +1,2 @@
+trainData='D:\\DevelopTool\\Workspace\\Git\\AIData\\mnist\\train\\mnist_train.csv'
+testData='D:\\DevelopTool\\Workspace\\Git\\AIData\\mnist\\test\\mnist_test.csv'
--- network/neural.py
+++ network/neural.py
@@ -18,16 +18,37 @@ class NeuralNetwork(object):
         l=[self.w_itoh,self.w_htoo]
         return l
 
-    def train(self):
-        pass
+    def train(self,inputs,targets):
+        targets=numpy.array(targets,ndmin=2).T
+        inputs=numpy.array(inputs,ndmin=2).T
+        #get hidden layer inputs and outputs
+        hidden_i=numpy.dot(self.w_itoh,inputs)
+        hidden_o=self.active_func(hidden_i)
+        #get output layse inputs and outputs
+        output_i=numpy.dot(self.w_htoo,hidden_o)
+        outputs=self.active_func(output_i)
+        #get output layer errors and hidden layers errors
+        o_e=targets-outputs
+        h_e=numpy.dot(self.w_htoo.T,o_e)
+        #get htoo weight changes
+        w_htoo_c=self.lr*numpy.dot(o_e*outputs*(1.0-outputs),numpy.transpose(hidden_o))
+        #get itoh weight changes
+        w_itoh_c=self.lr*numpy.dot(h_e*hidden_o*(1-hidden_o),numpy.transpose(inputs))
+
+        self.w_htoo+=w_htoo_c
+        self.w_itoh+=w_itoh_c
+        print(w_htoo_c)
+        print(w_itoh_c)
+
 
     def query(self,inputs):
         #initial inputs is initial outpus
-        outputs=inputs
+        outputs=numpy.array(inputs,ndmin=2).T
         for weight in self.weights:
             inputs=numpy.dot(weight,outputs)
             outputs=self.active_func(inputs)
         return outputs
+
 
 
 
@@ -37,4 +58,9 @@ hnodes=3
 onodes=3
 lr=0.5
 net=NeuralNetwork(inodes,hnodes,onodes,lr)
-print(net.query([1,2,3]))
+net.train([1,2,3],[0.9,0.9,0.9])
+#l1=[1,2,3]
+#l2=[1,2,3]
+#a1=numpy.array(l1)
+#a2=numpy.array(l2)
+#print(a1*a2)
